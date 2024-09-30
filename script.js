aa8139b7ec15a48b649168b891d92d21ce7a75a7
Arun katara <102404336+Jack09378@users.noreply.github.com>
2024-09-30 15:02:48 +0530

    app.js

--- script.js
+++ script.js
@@ -1,36 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Select the toggle icon and the container
-    const toggleIcon = document.querySelector('.toggle-icon');
-    const icons = document.querySelectorAll('.toggle-icon i');
-    const container = document.querySelector('.container');
+document.getElementById("downloadBtn").addEventListener("click", function () {
 
-    // Add event listener to toggle dark mode
-    toggleIcon.addEventListener('click', () => {
-        // Toggle dark mode class on the container
-        container.classList.toggle('dark-mode-active');
+    const link = document.createElement("a");
 
-        // Toggle the icon between moon and sun
-        icons.forEach(icon => {
-            icon.classList.toggle('bx-sun');
-        });
-    });
+    link.href = "cv.pdf";
+    link.download = "Arun_Kumar_CV.pdf";
+
+    document.body.appendChild(link);
+
+    link.click();
+
+    document.body.removeChild(link);
+    console.log("link")
 });
 
-
-
-// Download button
-
-document.getElementById('download').addEventListener('click', function () {
-    const fileUrl = 'E:\Arun\HTML\CODE\Project 1\file-example_PDF_1MB.pdf';
-
-    const filName = 'file-example_PDF_1MB.pdf';
-
-    const anchor = document.createElement('a');
-    anchor.href = fileUrl;
-    anchor.download = 'file-example_PDF_1MB.pdf';
-    document.body.appendChild(anchor);
-
-    anchor.click();
-
-    document.body.removeChild(anchor)
-})
